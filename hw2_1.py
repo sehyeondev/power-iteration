dc51a5730b815eae7baabe96d517331c9da24824
Sehyeon Kim <sue2368@gmail.com>
2021-10-19 17:37:21 +0900

    change form of value in map-reduce

--- hw2_1.py
+++ hw2_1.py
@@ -1,90 +1,133 @@
 import sys
-import math
+# import math
 from pyspark import SparkConf, SparkContext
 
-# get distance of two points
-def get_distance(l1, l2):
+## ======================================
+## basic functions for clustering
+## get_distance(point1, point2)
+## get_closest cluster(point_feature, k_init_features)
+## ======================================
+# get distance between two points
+def get_distance(point1, point2):
     sumsq = 0
-    for i in range(len(l1)):
-        sumsq += (float(l1[i]) - float(l2[i])) **2
-    return math.sqrt(sumsq)
+    for i in range(len(point1)):
+        sumsq += (float(point1[i]) - float(point2[i])) **2
+    return sumsq**0.5
 
+# get closest cluster for one point
+def get_closest_cluster(k_init_features, point):
+    point_feature = point.split()
+    distances = [get_distance(point_feature, init_f) for init_f in k_init_features]
+    result = [distances.index(min(distances)), point_feature]
+    return result
+
+## ============================================
+## initialize k points -> center of k clusters
+## cluster remaining points
+## ============================================
 # initialize k points
 def initialize_k_points(input_file, k_value):
     init_points = [0] # pick first point as first point in the dataset
-    # file = open(input_file, "r")
-    with open(input_file) as file:
-        lines = file.read().splitlines()
-        while len(init_points) < k_value:
-            max_dis = [10**9, 0]
-            for i in range(len(lines)):
-            # i = 0
-            # for line in file.readlines():
-                cand_point = lines[i].split()
-                min_dis = [10**9, 10**9]
-                for j in init_points:
-                    if i in init_points:
-                        continue
-                    dis = get_distance(cand_point, lines[j].split())
-                    if dis < min_dis[1]:
-                        min_dis[0] = i
-                        min_dis[1] = dis
-                    if min_dis[1] > max_dis[1]:
-                        max_dis[0] = min_dis[0]
-                        max_dis[1] = min_dis[1]
+    file = open(input_file, "r")
+    lines = file.readlines()
+    file.close()
+    while len(init_points) < k_value:
+        max_dis = [10**9, 0]
+        for i in range(len(lines)):
+            cand_point = lines[i].split()
+            min_dis = [10**9, 10**9]
+            for j in init_points:
+                if i in init_points:
+                    continue
+                dis = get_distance(cand_point, lines[j].split())
+                if dis <= min_dis[1]:
+                    min_dis[0] = i
+                    min_dis[1] = dis
+                if min_dis[1] >= max_dis[1]:
+                    max_dis[0] = min_dis[0]
+                    max_dis[1] = min_dis[1]
+                    if len(init_points) == 3 and i == 3896:
+                        print("if 3 points are added and i is 3896")
+                        print(max_dis, min_dis)
                 # i += 1
+            if (i == 3896):
+                print ("3896")
+                print (init_points)
+                print (max_dis, min_dis)
+            if (i == 3466):
+                print ("3466")
+                print (init_points)
+                print (max_dis, min_dis)
+            if (i == 3506):
+                print ("3506")
+                print (init_points)
+                print (max_dis, min_dis)
         init_points.append(max_dis[0])
-
-    return init_points
-    
-  
     return init_points
 
-# get distances from one point to k clusters
-def get_distances(line, k_init_features):
-    point_features = line.split()
-    distances = []
-    for i in range(k_init_features):
-        distances.append((i, get_distance(k_init_features[i], point_features)))
-    return distances
-
-# get closest cluster of each point
-def get_closest_cluster(k_dis):
-    select = 0
-    for i in range(1, len(k_dis)):
-        if k_dis[select][1] > k_dis[i][1]:
-            select = i
-    return (k_dis[i])
-
-# k-means algorithm
+# cluster remaining points
 def k_means(input_file, k_init_points):
+    # make lines as list
+    file = open(input_file, "r")
+    lines_list = file.readlines()
+    file.close()
+    # make lines for map-reduce
     conf = SparkConf()
     sc = SparkContext(conf=conf)
     lines = sc.textFile(input_file)
-    k_init_features = [lines[c_num].split for c_num in k_init_points]
-    all_distances = lines.map(lambda line: get_distances(line, k_init_features)) # (c_num, dis)
-    final_clusters = all_distances.flatMap(lambda k_dis: get_closest_cluster(k_dis))
-    return final_clusters
+    # cluster remaining points
+    k_init_features = [lines_list[c_num].split() for c_num in k_init_points]
+    cluster_pairs = lines.map(lambda point: get_closest_cluster(k_init_features, point))
+    clusters = cluster_pairs.groupByKey().mapValues(list)
+    # .collect()
+    return clusters
 
-# main function
-# [1] get input_file and k-value from command line argument
-# [2] initialize k clusters
-# [3] cluster remaining points
-# [4] get diameter of k clusters
-# [5] print average diameter
+## ==================================================
+## after clustering all points by k-means algorithm
+## find diameter of each cluster and average diameter
+## diameter: largest distance of any two points
+## ==================================================
+# get diameter from points in a cluster
+def get_diameter(points):
+    diameter = 0
+    for i in range(len(points)):
+        for j in range(i+1, len(points)):
+            dis = get_distance(points[i], points[j])
+            if dis > diameter:
+                diameter = dis
+    print (diameter)
+    return diameter
 
+# get average diameter of all clusters
+def get_avg_diameter(diameter_pairs):
+    sum_diameters = 0
+    for pair in diameter_pairs:
+        sum_diameters += pair[1]
+    avg_diameter = sum_diameters/len(diameter_pairs) # get average diameter
+    return avg_diameter
 
+## ==========================================================
+## main function
+## [1] get input_file and k-value from command line argument
+## [2] initialize k clusters
+## [3] cluster remaining points
+## [4] get diameter of k clusters
+## [5] print average diameter
+## ==========================================================
 def main():
     input_file = sys.argv[1] # get input file
-    # print (type(sys.argv[2]))
     k_value = int(sys.argv[2]) # get k-value
     k_init_points = initialize_k_points(input_file, k_value) # initialize k clusters
+    print("k_init_points")
     print (k_init_points)
     clusters = k_means(input_file, k_init_points) # cluster remaining points <- k-means algorithm
-    print (clusters.collect())
-    diameters = clusters.reduceBykey(lambda d_x, d_y: max(d_x, d_y)) # get diameter of all clusters
-    print (diameters.collect())
-    avg_diameter = float(sum(diameters))/float(len(diameters)) # get average diameter
+    # print ("clusters")
+    # print (clusters.collect())
+    diameter_pairs = clusters.mapValues(lambda points: get_diameter(points)).collect()
+    print("diameter_pairs")
+    print (diameter_pairs)
+    avg_diameter = get_avg_diameter(diameter_pairs)
+    print ("avg_diameter")
     print (avg_diameter)
 
 if __name__ == '__main__':
